78edbad66f43a696ddf75718ec57475e3a07c7c6
DUDIzark <113386382+DUDIzark@users.noreply.github.com>
2022-09-19 14:49:01 -0400

    python horrivel

---
+++ exercicio 1.py
@@ -0,0 +1,5 @@
+idade = int(input("insira o ano de nascimento: "))
+ano = int(input("digite o ano em que vc quer saber:"))
+
+aniv = ano - idade
+print("em", ano, "voce tera", aniv,"anos!")
---
+++ exercicio 2.py
@@ -0,0 +1,6 @@
+nome = 'pedro alvares cabral'
+idade = 900
+peso = 10,1
+print = (nome, idade, peso)
+
+
